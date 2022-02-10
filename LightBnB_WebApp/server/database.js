53ed4136bf5b0edd29d62e62718c0172cb6533f1
Paules2021 <P.elishaee@gmail.com>
2022-02-10 17:40:25 +0000

    see all reservation-update the reservation function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -57,15 +57,15 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser = function (user) {
   return pool
-  .query(`
+    .query(`
    INSERT INTO users (name, email, password)
    VALUES ($1, $2, $3)
    RETURNING *;
   `, [user.name, user.email, user.password])
-  .then((res) => {
-    return res.rows[0];
-  })
-  .catch((err) => err.message);
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => err.message);
 }
 exports.addUser = addUser;
 
@@ -77,7 +77,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `, [guest_id, limit])
+    .then((res) => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch((err) => err.message);
 }
 exports.getAllReservations = getAllReservations;
 
